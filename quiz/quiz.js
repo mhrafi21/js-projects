24cf08bd932e0867f246c63b8f29c1118c08bee4
mhrafi21 <12mhrafi@gmail.com>
2024-06-22 04:17:43 +0600

    add: quiz more functionality

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -34,28 +34,30 @@ const quizData = [
 ];
 
 const quiz = document.getElementById("quiz");
-
 const answerEls = document.querySelectorAll(".answer");
-
 const questionEl = document.getElementById("question");
-
 const a_text = document.getElementById("a_text");
-
 const b_text = document.getElementById("b_text");
-
 const c_text = document.getElementById("c_text");
-
 const d_text = document.getElementById("d_text");
-
 const submitBtn = document.getElementById("submit");
+const timer = document.getElementById("time");
+const notSelect = document.getElementById("notSelectError");
+const currentQ = document.getElementById("current");
+const totalQuiz = document.getElementById("total")
 
 let currentQuiz = 0;
 
 let score = 0;
 
+loadQuiz();
+QuizCount();
+startTimer();
+
+console.log(currentQuiz);
+
 function loadQuiz() {
   deselectAnswer();
-
   const currentQuizData = quizData[currentQuiz];
 
   questionEl.innerText = currentQuizData.question;
@@ -65,8 +67,13 @@ function loadQuiz() {
   d_text.innerText = currentQuizData.d;
 }
 
+function QuizCount() {
+    totalQuiz.innerText = quizData.length;
+    currentQ.innerText = currentQuiz + 1;
+}
+
 function deselectAnswer() {
-  answerEls.forEach((ansEl) => ansEl.checked === false);
+  answerEls.forEach((ansEl) => (ansEl.checked = false));
 }
 
 function getSelected() {
@@ -75,24 +82,67 @@ function getSelected() {
   answerEls.forEach((ansEl) => {
     if (ansEl.checked) {
       answer = ansEl.id;
+      enableSubmitButton();
+      notSelect.innerText = "";
     }
   });
-
   return answer;
 }
 
+function startTimer() {
+  const timerInterval = setInterval(() => {
+    timeLeft--;
+    timer.innerText = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      disableSubmitButton();
+      submitBtn.click();
+      return (notSelect.innerText = "Time is over!");
+    }
+  }, 1000);
+
+}
+
+function disableSubmitButton() {
+    submitBtn.disabled = true;
+    submitBtn.style.cursor= "not-allowed"
+    submitBtn.style.backgroundColor = '#cccccc'; 
+}
+
+function enableSubmitButton() {
+    submitBtn.disabled = false;
+    submitBtn.style.backgroundColor = '#4CAF50'; 
+}
+
+function QuizCount() {
+    currentQ.innerText = currentQuiz;
+    totalQuiz.innerText = quizData.length;
+}
+
+
+
 submitBtn.addEventListener("click", () => {
   const answer = getSelected();
+
+  if (!answer) {
+    return (notSelect.innerText = "Please Select One of them");
+  } else {
+    notSelect.innerText = "";
+    
+  }
   if (answer === quizData[currentQuiz].correct) {
     score++;
   }
   currentQuiz++;
+  timeLeft = 10;
 
   if (currentQuiz < quizData.length) {
     loadQuiz();
   } else {
-    quiz.innerHTML = `<h2>Your answered Score ${score}/${quizData.length} questions correctly</h2>
+    quiz.innerHTML = `<h2>Your answered Score ${score} out of ${quizData.length}</h2>
     <button onclick="location.reload()">Reload</button>
     `;
   }
 });
+
