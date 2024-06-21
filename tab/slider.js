32cbf4533e0ee15023d9fadb311999733ba6ba33
mhrafi21 <12mhrafi@gmail.com>
2024-06-22 02:02:06 +0600

    solved clickable bg color

--- tab/slider.js
+++ tab/slider.js
@@ -2,7 +2,6 @@ function openTab(event, tabName) {
   let i, tabContent, tabLinks;
 
   // Get all elements with class="tab-content" and hide them
-
   tabContent = document.getElementsByClassName("tab-content");
   for (i = 0; i < tabContent.length; i++) {
     tabContent[i].classList.remove("active");
@@ -11,17 +10,19 @@ function openTab(event, tabName) {
   tabLinks = document.getElementsByClassName("tab-links");
 
   for (i = 0; i < tabLinks.length; i++) {
+    tabLinks[i].classList.add("active");
     tabLinks[i].classList.remove("active");
+    
   }
 
 
   document.getElementById(tabName).classList.add("active");
-  event.currentTarget.classList.add("active");
+  event.target.classList.add("active");
+  // Add the active class to the current/clicked tab and remove the "active" class from the previous/open tab
 
-
+  
 }
 
 // by default, show the first tab;
-
 document.getElementById("tab1").classList.add("active");
 document.querySelector(".tab-links").classList.add("active")
